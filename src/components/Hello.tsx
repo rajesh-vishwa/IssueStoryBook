898dcd8ed042ce8c8a9f8073e75f495a6590c339
RV <sms.rajesh@gmail.com>
2022-09-29 09:09:32 +0530

    install and add sample component

---
+++ src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Hello({}: Props) {
+  const someObject: any = { a: 1 };
+  //if (someObject?.b) {
+  //}
+
+  return <div>Hello</div>;
+}
